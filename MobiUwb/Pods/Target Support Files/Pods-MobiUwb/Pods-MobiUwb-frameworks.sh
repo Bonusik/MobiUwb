8091e10abaa2b69542fae53efa2d8ff41ed38e41
Grzegorz Szymanski <grzegorz@qburst.com>
2016-05-16 10:45:39 +0200

    contact view controller with button functionality

--- MobiUwb/Pods/Target Support Files/Pods-MobiUwb/Pods-MobiUwb-frameworks.sh
+++ MobiUwb/Pods/Target Support Files/Pods-MobiUwb/Pods-MobiUwb-frameworks.sh
@@ -84,11 +84,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-MobiUwb/MBProgressHUD.framework"
   install_framework "Pods-MobiUwb/SWXMLHash.framework"
   install_framework "Pods-MobiUwb/SlideMenuControllerSwift.framework"
   install_framework "Pods-MobiUwb/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-MobiUwb/MBProgressHUD.framework"
   install_framework "Pods-MobiUwb/SWXMLHash.framework"
   install_framework "Pods-MobiUwb/SlideMenuControllerSwift.framework"
   install_framework "Pods-MobiUwb/SwiftyJSON.framework"
